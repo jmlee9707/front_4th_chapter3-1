1dd0df870d5f7cff01a65a3dbd45ba16bd355926
jmlee9707 <jmlee9707@naver.com>
2025-02-07 00:13:07 +0900

    feat: time validation test code 작성

--- src/__tests__/unit/easy.timeValidation.spec.ts
+++ src/__tests__/unit/easy.timeValidation.spec.ts
@@ -1,15 +1,34 @@
 import { getTimeErrorMessage } from '../../utils/timeValidation';
 
 describe('getTimeErrorMessage >', () => {
-  it('시작 시간이 종료 시간보다 늦을 때 에러 메시지를 반환한다', () => {});
+  const errorMessage = {
+    startTimeError: '시작 시간은 종료 시간보다 빨라야 합니다.',
+    endTimeError: '종료 시간은 시작 시간보다 늦어야 합니다.',
+  };
+  const noneErrorMessage = { startTimeError: null, endTimeError: null };
 
-  it('시작 시간과 종료 시간이 같을 때 에러 메시지를 반환한다', () => {});
+  it('시작 시간이 종료 시간보다 늦을 때 에러 메시지를 반환한다', () => {
+    expect(getTimeErrorMessage('16:00', '15:00')).toEqual(errorMessage);
+  });
 
-  it('시작 시간이 종료 시간보다 빠를 때 null을 반환한다', () => {});
+  it('시작 시간과 종료 시간이 같을 때 에러 메시지를 반환한다', () => {
+    expect(getTimeErrorMessage('16:00', '16:00')).toEqual(errorMessage);
+  });
 
-  it('시작 시간이 비어있을 때 null을 반환한다', () => {});
+  it('시작 시간이 종료 시간보다 빠를 때 null을 반환한다', () => {
+    expect(getTimeErrorMessage('12:00', '15:00').startTimeError).toBeNull();
+    expect(getTimeErrorMessage('12:00', '15:00').endTimeError).toBeNull();
+  });
 
-  it('종료 시간이 비어있을 때 null을 반환한다', () => {});
+  it('시작 시간이 비어있을 때 null을 반환한다', () => {
+    expect(getTimeErrorMessage('', '15:00')).toEqual(noneErrorMessage);
+  });
 
-  it('시작 시간과 종료 시간이 모두 비어있을 때 null을 반환한다', () => {});
+  it('종료 시간이 비어있을 때 null을 반환한다', () => {
+    expect(getTimeErrorMessage('17:00', '')).toEqual(noneErrorMessage);
+  });
+
+  it('시작 시간과 종료 시간이 모두 비어있을 때 null을 반환한다', () => {
+    expect(getTimeErrorMessage('', '')).toEqual(noneErrorMessage);
+  });
 });
